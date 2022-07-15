ee40feef60b953c32dd11754d747f691fbff6b82
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-07-14 20:38:49 -0400

    border-left for children

--- BlazorStudio.RazorLib/TreeView/TreeViewDisplay.razor.cs
+++ BlazorStudio.RazorLib/TreeView/TreeViewDisplay.razor.cs
@@ -13,9 +13,16 @@ public partial class TreeViewDisplay<T>
     public Func<T, Task<IEnumerable<T>>> GetChildrenFunc { get; set; } = null!;
     [CascadingParameter]
     public RenderFragment<T> ChildContent { get; set; } = null!;
+    /// <summary>
+    /// Depth starts at 0
+    /// </summary>
+    [CascadingParameter(Name="Depth")]
+    public int Depth { get; set; }
 
     [Parameter, EditorRequired]
     public TreeView<T> TreeView { get; set; } = null!;
+
+    private const int DEPTH_PADDING_LEFT_SCALING_IN_PIXELS = 12;
 
     /// <summary>
     /// This is used to ensure Children are only loaded once
@@ -24,7 +31,9 @@ public partial class TreeViewDisplay<T>
     /// A separate way to force refresh children needs to exist
     /// </summary>
     private bool _shouldLoadChildren = true;
-    
+
+    private string GetScaledByDepthPixelsOffset(int depth) => $"{depth * DEPTH_PADDING_LEFT_SCALING_IN_PIXELS}px;";
+
     private async Task GetChildrenAsync()
     {
         TreeView.Children = (await GetChildrenFunc(TreeView.Item))
