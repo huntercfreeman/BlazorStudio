1f1c877f038a3874e9703793b9c669de0a9b43d1
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-07-15 22:55:56 -0400

    ArrowLeft and ArrowRight for TreeView

--- BlazorStudio.RazorLib/TreeView/TreeViewDisplay.razor.cs
+++ BlazorStudio.RazorLib/TreeView/TreeViewDisplay.razor.cs
@@ -3,6 +3,8 @@ using BlazorStudio.ClassLib.Store.TreeViewCase;
 using Fluxor;
 using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using PlainTextEditor.ClassLib.Keyboard;
 
 namespace BlazorStudio.RazorLib.TreeView;
 
@@ -28,6 +30,12 @@ public partial class TreeViewDisplay<T>
 
     [Parameter, EditorRequired]
     public TreeView<T> TreeView { get; set; } = null!;
+    [Parameter, EditorRequired]
+    public Func<ITreeView[]> GetSiblingsAndSelfFunc { get; set; } = null!;
+    [Parameter, EditorRequired]
+    public int IndexAmongSiblings { get; set; }
+    [Parameter, EditorRequired]
+    public TreeView<T>? Parent { get; set; }
 
     private const int DEPTH_PADDING_LEFT_SCALING_IN_PIXELS = 12;
 
@@ -39,6 +47,8 @@ public partial class TreeViewDisplay<T>
     /// </summary>
     private bool _shouldLoadChildren = true;
     private ElementReference _titleSpan;
+    private bool _isGettingChildren;
+    private bool _previousFocusState;
 
     protected override void OnInitialized()
     {
@@ -50,6 +60,20 @@ public partial class TreeViewDisplay<T>
         });
 
         base.OnInitialized();
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        var localIsActive = IsActive;
+
+        if (_previousFocusState != localIsActive)
+        {
+            _previousFocusState = localIsActive;
+
+            _titleSpan.FocusAsync();
+        }
+
+        base.OnAfterRender(firstRender);
     }
 
     private bool IsActive => TreeViewWrapStateSelection.Value?.ActiveTreeViews
@@ -69,6 +93,7 @@ public partial class TreeViewDisplay<T>
             .ToArray();
 
         _shouldLoadChildren = false;
+        _isGettingChildren = false;
 
         await InvokeAsync(StateHasChanged);
     }
@@ -77,7 +102,12 @@ public partial class TreeViewDisplay<T>
     {
         if (_shouldLoadChildren)
         {
-            _ = Task.Run(async () => await GetChildrenAsync());
+            _ = Task.Run(async () =>
+            {
+                _isGettingChildren = true;
+                await GetChildrenAsync();
+                _isGettingChildren = false;
+            });
         }
 
         TreeView.IsExpanded = !TreeView.IsExpanded;
@@ -88,5 +118,48 @@ public partial class TreeViewDisplay<T>
         Dispatcher.Dispatch(new SetActiveTreeViewAction(TreeViewWrapKey, TreeView));
 
         _titleSpan.FocusAsync();
+    }
+    
+    private void HandleOnKeyDown(KeyboardEventArgs keyboardEvent)
+    {
+        if (_previousFocusState == false)
+            return;
+
+        _previousFocusState = false;
+
+        if (keyboardEvent.Key == KeyboardKeyFacts.MovementKeys.ARROW_RIGHT_KEY)
+        {
+            if (!TreeView.IsExpanded)
+            {
+                ToggleIsExpandedOnClick();
+            }
+            else
+            {
+                var children = TreeView.Children;
+
+                if (children.Length > 0)
+                {
+                    Dispatcher.Dispatch(new SetActiveTreeViewAction(TreeViewWrapKey, children[0]));
+                }
+            }
+        }
+        else if (keyboardEvent.Key == KeyboardKeyFacts.MovementKeys.ARROW_LEFT_KEY)
+        {
+            if (TreeView.IsExpanded)
+            {
+                ToggleIsExpandedOnClick();
+            }
+            else
+            {
+                if (Parent is not null)
+                {
+                    Dispatcher.Dispatch(new SetActiveTreeViewAction(TreeViewWrapKey, Parent));
+                }
+            }
+        }
+        else
+        {
+            _previousFocusState = true;
+        }
     }
 }
