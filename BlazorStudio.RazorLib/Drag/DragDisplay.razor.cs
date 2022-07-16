57f7cf79c8afd2d11819c93532dbf068d1d02d3e
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-07-16 11:32:23 -0400

    Working on Resize

--- BlazorStudio.RazorLib/Drag/DragDisplay.razor.cs
+++ BlazorStudio.RazorLib/Drag/DragDisplay.razor.cs
@@ -21,26 +21,6 @@ public partial class DragDisplay : FluxorComponent
     private SetDragStateAction ConstructClearDragStateAction() => 
         new SetDragStateAction(false, new MouseEventArgs());
 
-    private bool _previousIsDisplayed;
-    private ElementReference _dragDisplayElement;
-
-    protected override void OnAfterRender(bool firstRender)
-    {
-        var dragState = DragStateWrap.Value;
-
-        if (_previousIsDisplayed != dragState.IsDisplayed)
-        {
-            _previousIsDisplayed = dragState.IsDisplayed;
-
-            if (dragState.IsDisplayed)
-            {
-                _dragDisplayElement.FocusAsync();
-            }
-        }
-
-        base.OnAfterRender(firstRender);
-    }
-
     private void DispatchNotifyMouseEventOnMouseMove(MouseEventArgs mouseEventArgs)
     {
         if (mouseEventArgs.Button != MouseFacts.MOUSE_LEFT_BUTTON)
@@ -54,11 +34,6 @@ public partial class DragDisplay : FluxorComponent
     }
     
     private void DispatchHideOnMouseUp()
-    {
-        Dispatcher.Dispatch(ConstructClearDragStateAction());
-    }
-    
-    private void DispatchHideOnFocusOut()
     {
         Dispatcher.Dispatch(ConstructClearDragStateAction());
     }
