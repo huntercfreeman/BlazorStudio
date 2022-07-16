4f4e88d3630546d19f3d16d0b00b8dd6786a1a43
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-07-16 02:45:15 -0400

    Dialog only show 1 time

--- BlazorStudio.RazorLib/Settings/SettingsDialogEntryPoint.razor.cs
+++ BlazorStudio.RazorLib/Settings/SettingsDialogEntryPoint.razor.cs
@@ -7,15 +7,20 @@ namespace BlazorStudio.RazorLib.Settings;
 public partial class SettingsDialogEntryPoint : ComponentBase
 {
     [Inject]
+    private IState<DialogStates> DialogStatesWrap { get; set; } = null!;
+    [Inject]
     private IDispatcher Dispatcher { get; set; } = null!;
+
+    private DialogRecord _settingsDialog = new DialogRecord(
+        DialogKey.NewDialogKey(),
+        "Settings",
+        typeof(SettingsDialog),
+        null
+    );
 
     private void OpenSettingsDialogOnClick()
     {
-        Dispatcher.Dispatch(new RegisterDialogAction(new DialogRecord(
-            DialogKey.NewDialogKey(), 
-            "Settings",
-            typeof(SettingsDialog),
-            null
-        )));
+        if (!DialogStatesWrap.Value.List.Contains(_settingsDialog))
+            Dispatcher.Dispatch(new RegisterDialogAction(_settingsDialog));
     }
 }
