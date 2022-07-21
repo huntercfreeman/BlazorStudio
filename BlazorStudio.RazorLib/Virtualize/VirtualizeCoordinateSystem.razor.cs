0ac190d12891936561e71f1f1d2c9a58bb2175d8
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-07-21 03:18:51 -0400

    Working on VirtualizeCoordinateSystem

---
+++ BlazorStudio.RazorLib/Virtualize/VirtualizeCoordinateSystem.razor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Immutable;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorStudio.RazorLib.Virtualize;
+
+public partial class VirtualizeCoordinateSystem<T> : ComponentBase, IDisposable
+{
+    /// <summary>
+    /// Used when scrolling and the cached content runs out and different cache needs loading
+    /// </summary>
+    [Parameter, EditorRequired]
+    public Action<CancellationToken> RequestCallbackAction { get; set; } = null!;
+
+    private CancellationTokenSource _cancellationTokenSource = new();
+    private ImmutableArray<T> _data = ImmutableArray<T>.Empty;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+    }
+
+    public void FireRequestCallbackAction()
+    {
+        RequestCallbackAction(CancelTokenSourceAndGetNewToken());
+    }
+    
+    public void SetData(IEnumerable<T> data)
+    {
+        _data = data.ToImmutableArray();
+
+        InvokeAsync(StateHasChanged);
+    }
+    
+    public CancellationToken CancelTokenSourceAndGetNewToken()
+    {
+        _cancellationTokenSource.Cancel();
+        _cancellationTokenSource = new();
+
+        return _cancellationTokenSource.Token;
+    }
+
+    public void Dispose()
+    {
+        _cancellationTokenSource?.Cancel();
+    }
+}
