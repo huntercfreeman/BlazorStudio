8970fcd5ef63be48212b787eb31cb6919d9f4273
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-07-24 13:34:40 -0400

    Properly alter open Tab Index when closing a Tab

--- BlazorStudio.RazorLib/Editor/EditorDisplay.razor.cs
+++ BlazorStudio.RazorLib/Editor/EditorDisplay.razor.cs
@@ -27,6 +27,24 @@ public partial class EditorDisplay : FluxorComponent
     
     private void DisposePlainTextEditorOnClick(PlainTextEditorKey plainTextEditorKey)
     {
+        var plainTextEditorStates = PlainTextEditorStatesWrap.Value;
+        var editorState = EditorStateWrap.Value;
+
+        // -1 for the to be removed plainTextEditorKey
+        // and -1 again to start index from 0
+        if (editorState.TabIndex > plainTextEditorStates.Array.Length - 2)
+        {
+            // Out of bounds of the upcoming Array length
+
+            var nextTabIndex = plainTextEditorStates.Array.Length - 2;
+
+            nextTabIndex = nextTabIndex < 0
+                ? 0
+                : nextTabIndex;
+
+            Dispatcher.Dispatch(new SetActiveTabIndexAction(nextTabIndex));
+        }
+        
         Dispatcher.Dispatch(new DeconstructPlainTextEditorRecordAction(plainTextEditorKey));
     }
 }
