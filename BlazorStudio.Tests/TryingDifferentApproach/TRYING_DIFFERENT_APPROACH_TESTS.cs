69b18f7d975d65aaa3118041579ca4e3a87aa273
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-07-28 09:27:03 -0400

    Start of trying a different approach

--- BlazorStudio.Tests/TryingDifferentApproach/TRYING_DIFFERENT_APPROACH_TESTS.cs
+++ BlazorStudio.Tests/TryingDifferentApproach/TRYING_DIFFERENT_APPROACH_TESTS.cs
@@ -11,51 +11,8 @@ public class TRYING_DIFFERENT_APPROACH_TESTS : PLAIN_TEXT_EDITOR_STATES_TESTS
         var inputAbsoluteFilePath = new AbsoluteFilePath(AbsoluteFilePathToThisCSharpProject +
                                                          "CacheTests\\THE TELL-TALE HEART.txt",
             false);
-
-        var requestOne = new FileCoordinateGridRequest(
-            0,
-            30,
-            0,
-            50,
-            CancellationToken.None);
-
-        var requestTwo = new FileCoordinateGridRequest(
-            10,
-            10,
-            10,
-            20,
-            CancellationToken.None);
-
-        var expectedResultOne = File.ReadAllText(AbsoluteFilePathToThisCSharpProject +
-                                                 "CacheTestsData\\CompleteOverlap\\ChunkEncompassesRequest\\resultOne_CompleteOverlapChunkEncompassesRequest.txt");
-
-        var expectedResultTwo = File.ReadAllText(AbsoluteFilePathToThisCSharpProject +
-                                                 "CacheTestsData\\CompleteOverlap\\ChunkEncompassesRequest\\resultTwo_CompleteOverlapChunkEncompassesRequest.txt");
-
+        
         var plainTextEditorKey = PlainTextEditorKey.NewPlainTextEditorKey();
-
-        Dispatcher.Dispatch(new ConstructMemoryMappedFilePlainTextEditorRecordAction(plainTextEditorKey,
-            inputAbsoluteFilePath));
-
-        var plainTextEditor = State.Value.Map[plainTextEditorKey];
-
-        var actionOne = new MemoryMappedFileExactReadRequestAction(plainTextEditorKey,
-            requestOne);
-
-        Dispatcher.Dispatch(actionOne);
-
-        var actualResultOne = plainTextEditor.GetPlainText();
-
-        Assert.Equal(expectedResultOne, actualResultOne);
-
-        var actionTwo = new MemoryMappedFileExactReadRequestAction(plainTextEditorKey,
-            requestTwo);
-
-        Dispatcher.Dispatch(actionTwo);
-
-        var actualResultTwo = plainTextEditor.GetPlainText();
-
-        Assert.Equal(expectedResultTwo, actualResultTwo);
     }
     
     [Fact]
