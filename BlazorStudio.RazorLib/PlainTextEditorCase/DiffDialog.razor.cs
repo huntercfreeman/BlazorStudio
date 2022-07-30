c809c9767a6b1f3b2195d9c91be1527d9e56cdbf
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-07-30 12:13:16 -0400

    Starting diff dialog

---
+++ BlazorStudio.RazorLib/PlainTextEditorCase/DiffDialog.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorStudio.RazorLib.PlainTextEditorCase;
+
+public partial class DiffDialog : ComponentBase
+{
+
+}
---
+++ BlazorStudio.RazorLib/PlainTextEditorCase/DiffDialogEntryPoint.razor.cs
@@ -0,0 +1,31 @@
+﻿using BlazorStudio.ClassLib.Store.DialogCase;
+using BlazorStudio.ClassLib.Store.PlainTextEditorCase;
+using BlazorStudio.RazorLib.Settings;
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorStudio.RazorLib.PlainTextEditorCase;
+
+public partial class DiffDialogEntryPoint : ComponentBase
+{
+    [Inject]
+    private IState<DialogStates> DialogStatesWrap { get; set; } = null!;
+    [Inject]
+    private IDispatcher Dispatcher { get; set; } = null!;
+
+    [Parameter, EditorRequired]
+    public PlainTextEditorKey PlainTextEditorKey { get; set; } = null!;
+
+    private readonly DialogRecord _diffDialog = new DialogRecord(
+        DialogKey.NewDialogKey(),
+        "Diff",
+        typeof(DiffDialog),
+        null
+    );
+
+    private void OpenDiffDialogOnClick()
+    {
+        if (DialogStatesWrap.Value.List.All(x => x.DialogKey != _diffDialog.DialogKey))
+            Dispatcher.Dispatch(new RegisterDialogAction(_diffDialog));
+    }
+}
