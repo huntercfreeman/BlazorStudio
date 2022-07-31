0397e178d98c968991b1f9613717b0ffa905ca56
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-07-31 17:24:33 -0400

    Moved out some markup from PlainTextEditorDisplay.razor to separate components and am working on footer

---
+++ BlazorStudio.RazorLib/PlainTextEditorCase/PlainTextEditorFooter.razor.cs
@@ -0,0 +1,10 @@
+﻿using BlazorStudio.ClassLib.Store.PlainTextEditorCase;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorStudio.RazorLib.PlainTextEditorCase;
+
+public partial class PlainTextEditorFooter : ComponentBase
+{
+    [Parameter]
+    public IPlainTextEditor PlainTextEditor { get; set; } = null!;
+}
---
+++ BlazorStudio.RazorLib/PlainTextEditorCase/PlainTextEditorHeader.razor.cs
@@ -0,0 +1,10 @@
+﻿using BlazorStudio.ClassLib.Store.PlainTextEditorCase;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorStudio.RazorLib.PlainTextEditorCase;
+
+public partial class PlainTextEditorHeader : ComponentBase
+{
+    [Parameter]
+    public IPlainTextEditor PlainTextEditor { get; set; } = null!;
+}
