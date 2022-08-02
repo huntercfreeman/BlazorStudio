688a016ff2df56a440568e9b4e966a1fb5fdeb29
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-08-02 01:03:48 -0400

    Added a lot of debug dump to UI

---
+++ BlazorStudio.RazorLib/DebuggingHelp/DebugExpansion.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorStudio.RazorLib.DebuggingHelp;
+
+public partial class DebugExpansion : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public string Title { get; set; } = null!;
+    [Parameter, EditorRequired]
+    public RenderFragment ChildContent { get; set; } = null!;
+
+    private bool _isExpanded;
+}
