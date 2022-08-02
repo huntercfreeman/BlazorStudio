82cd725b456ccad31f6797d88c3f28ea574f2357
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-08-01 21:38:20 -0400

    PreviouslySetCharacterColumnIndex

--- BlazorStudio.ClassLib/Store/PlainTextEditorCase/IPlainTextEditor.cs
+++ BlazorStudio.ClassLib/Store/PlainTextEditorCase/IPlainTextEditor.cs
@@ -8,22 +8,59 @@ namespace BlazorStudio.ClassLib.Store.PlainTextEditorCase;
 
 public interface IPlainTextEditor
 {
+    /// <summary>
+    /// A unique identifier for a given <see cref="IPlainTextEditor"/>
+    /// </summary>
     public PlainTextEditorKey PlainTextEditorKey { get; } 
+    /// <summary>
+    /// 
+    /// </summary>
     public SequenceKey SequenceKey { get; } 
     public ImmutableList<IPlainTextEditorRow> List { get; }
-    public int CurrentRowIndex { get; }
-    public int CurrentTokenIndex { get; }
     public IFileHandle? FileHandle { get; }
     public RichTextEditorOptions RichTextEditorOptions { get; }
-    public int LongestRowCharacterLength { get; }
     public VirtualizeCoordinateSystemMessage VirtualizeCoordinateSystemMessage { get; }
     public FileHandleReadRequest FileHandleReadRequest { get; init; }
-    public int CharacterIndexOffsetRelativeToRow { get; }
     public bool IsReadonly { get; }
     public bool UseCarriageReturnNewLine { get; }
-
-    // New things
-
+    /// <summary>
+    /// This is the row index relative to the small section of a larger file
+    /// that was read into a PlainTextEditor.
+    ///
+    /// To get the row index relative to the actual file one must
+    /// add the <see cref="RowIndexOffset"/>
+    /// </summary>
+    public int CurrentRowIndex { get; }
+    /// <summary>
+    /// This is the index of the active token relative to the current row.
+    ///
+    /// Not to be confused with <see cref="CurrentCharacterColumnIndex"/>
+    /// </summary>
+    public int CurrentTokenIndex { get; }
+    /// <summary>
+    /// This is the index of the primary cursor relative to the current row.
+    /// </summary>
+    public int CurrentCharacterColumnIndex { get; }
+    /// <summary>
+    /// This is the index of the primary cursor relative to the flat entirety of
+    /// the file itself (no rows involved).
+    /// </summary>
+    public int CurrentPositionIndex { get; }
+    /// <summary>
+    /// Similar to the <see cref="CurrentCharacterColumnIndex"/> this is
+    /// the index of the primary cursor relative to the current row.
+    /// <br/><br/>
+    /// However, this is mostly only set when the user hits on their keyboard the keys:
+    /// 'ArrowLeft', 'ArrowRight'.
+    /// <br/><br/>
+    /// The usage of this is for when the user hits on their keyboard the keys:
+    /// 'ArrowUp', 'ArrowDown'.
+    /// <br/><br/>
+    /// 'ArrowUp', 'ArrowDown' will set <see cref="CurrentCharacterColumnIndex"/> to the minimum
+    /// of <see cref="PreviouslySetCharacterColumnIndex"/> and the maximum character
+    /// column index of the row traveled to.
+    /// </summary>
+    public int PreviouslySetCharacterColumnIndex { get; }
     /// <summary>
     /// The PlainTextEditor only loads in small sections of a larger file.
     /// <br/><br/>
@@ -50,7 +87,7 @@ public interface IPlainTextEditor
     /// <br/><br/>
     /// In the previous example the <see cref="RowIndexOffset"/> would be 99
     /// </summary>
-    public int ColumnIndexOffset { get; }
+    public int CharacterColumnIndexOffset { get; }
 
     public string GetPlainText();
 }
