8cc6a4caaca6be771cf76b28bf8720db21dc0c76
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-08-05 05:26:08 -0400

    IconFile

---
+++ BlazorStudio.RazorLib/SolutionExplorer/SolutionExplorerIconDisplay.razor.cs
@@ -0,0 +1,10 @@
+﻿using BlazorStudio.ClassLib.FileSystem.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorStudio.RazorLib.SolutionExplorer;
+
+public partial class SolutionExplorerIconDisplay : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public IAbsoluteFilePath AbsoluteFilePath { get; set; } = null!;
+}
