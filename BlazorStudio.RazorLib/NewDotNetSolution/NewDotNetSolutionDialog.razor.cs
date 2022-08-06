36070555df006f52cd01a8da1580668b596776ae
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-08-06 11:06:15 -0400

    Fix for some css issues in solution dialog

--- BlazorStudio.RazorLib/NewDotNetSolution/NewDotNetSolutionDialog.razor.cs
+++ BlazorStudio.RazorLib/NewDotNetSolution/NewDotNetSolutionDialog.razor.cs
@@ -1,8 +1,24 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorStudio.ClassLib.FileSystem.Interfaces;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorStudio.RazorLib.NewDotNetSolution;
 
 public partial class NewDotNetSolutionDialog : ComponentBase
 {
     private string _solutionName = string.Empty;
+    private bool _disableExecuteButton;
+    private bool _finishedCreatingProject;
+    private IAbsoluteFilePath? InputFileDialogSelection;
+
+    private void InputFileDialogOnEnterKeyDownOverride((IAbsoluteFilePath absoluteFilePath, Action toggleIsExpanded) tupleArgument)
+    {
+        if (_disableExecuteButton || _finishedCreatingProject)
+            return;
+
+        if (tupleArgument.absoluteFilePath.IsDirectory)
+        {
+            InputFileDialogSelection = tupleArgument.absoluteFilePath;
+            InvokeAsync(StateHasChanged);
+        }
+    }
 }
