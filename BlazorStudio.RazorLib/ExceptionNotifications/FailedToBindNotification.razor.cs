13c3b670f4d828362e5e0b9c2e0ed5701a88361c
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-08-08 11:10:18 -0400

    Failed to bind notification no purpose yet

---
+++ BlazorStudio.RazorLib/ExceptionNotifications/FailedToBindNotification.razor.cs
@@ -0,0 +1,10 @@
+﻿using BlazorStudio.ClassLib.Store.NotificationCase;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorStudio.RazorLib.ExceptionNotifications;
+
+public partial class FailedToBindNotification : ComponentBase
+{
+    [CascadingParameter]
+    public NotificationRecord NotificationRecord { get; set; } = null!;
+}
