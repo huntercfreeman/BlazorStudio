fe32ab59dfadb93ca8c3d97033fad3c0db765fd8
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-08-08 10:22:58 -0400

    MsBuildAbsoluteFilePath

--- BlazorStudio.ClassLib/Store/SolutionCase/SolutionReducer.cs
+++ BlazorStudio.ClassLib/Store/SolutionCase/SolutionReducer.cs
@@ -1,4 +1,5 @@
-﻿using Fluxor;
+﻿using BlazorStudio.ClassLib.FileSystem.Classes;
+using Fluxor;
 
 namespace BlazorStudio.ClassLib.Store.SolutionCase;
 
@@ -8,6 +9,16 @@ public class SolutionReducer
     public static SolutionState ReduceSetSolutionWorkspaceAction(SolutionState previousSolutionState,
         SetSolutionAction setSolutionAction)
     {
-        return new(setSolutionAction.SolutionWorkspace, setSolutionAction.VisualStudioInstance);
+        var msBuildPathString = setSolutionAction.VisualStudioInstance.MSBuildPath;
+
+        if (msBuildPathString.EndsWith(Path.DirectorySeparatorChar) ||
+            msBuildPathString.EndsWith(Path.AltDirectorySeparatorChar))
+        {
+            msBuildPathString = msBuildPathString.Substring(0, msBuildPathString.Length - 1);
+        }
+
+        var msBuildAbsoluteFilePath = new AbsoluteFilePath(msBuildPathString, true);
+
+        return new(setSolutionAction.SolutionWorkspace, setSolutionAction.VisualStudioInstance, msBuildAbsoluteFilePath);
     }
 }
--- BlazorStudio.ClassLib/Store/SolutionCase/SolutionState.cs
+++ BlazorStudio.ClassLib/Store/SolutionCase/SolutionState.cs
@@ -1,13 +1,16 @@
-﻿using Fluxor;
+﻿using BlazorStudio.ClassLib.FileSystem.Classes;
+using BlazorStudio.ClassLib.FileSystem.Interfaces;
+using Fluxor;
 using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis.MSBuild;
 
 namespace BlazorStudio.ClassLib.Store.SolutionCase;
 
 [FeatureState]
-public record SolutionState(MSBuildWorkspace? SolutionWorkspace, VisualStudioInstance? VisualStudioInstance)
+public record SolutionState(MSBuildWorkspace? SolutionWorkspace, VisualStudioInstance? VisualStudioInstance,
+    IAbsoluteFilePath? MsBuildAbsoluteFilePath)
 {
-    private SolutionState() : this(default(MSBuildWorkspace), default(VisualStudioInstance))
+    private SolutionState() : this(default(MSBuildWorkspace), default(VisualStudioInstance), default(IAbsoluteFilePath))
     {
 
     }
