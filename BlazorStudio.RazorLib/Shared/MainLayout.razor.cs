5e321cff5fff475bc77453df2d91823372872073
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-08-08 10:50:08 -0400

    basic notification system

--- BlazorStudio.RazorLib/Shared/MainLayout.razor.cs
+++ BlazorStudio.RazorLib/Shared/MainLayout.razor.cs
@@ -1,4 +1,6 @@
-﻿using BlazorStudio.ClassLib.Store.ThemeCase;
+﻿using BlazorStudio.ClassLib.Store.NotificationCase;
+using BlazorStudio.ClassLib.Store.ThemeCase;
+using BlazorStudio.RazorLib.Notification;
 using Fluxor;
 using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
@@ -9,4 +11,16 @@ public partial class MainLayout : FluxorLayout
 {
     [Inject]
     private IState<ThemeState> ThemeStateWrap { get; set; } = null!;
+    [Inject]
+    private IDispatcher Dispatcher { get; set; } = null!;
+
+    private void TestNotificationOnClick()
+    {
+        var notificationRecord = new NotificationRecord(NotificationKey.NewNotificationKey(),
+            "Test Notification",
+            typeof(TestNotificationDisplay),
+            null);
+
+        Dispatcher.Dispatch(new RegisterNotificationAction(notificationRecord));
+    }
 }
