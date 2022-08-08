dd1f6b4f29fb233b33a014bc37cbed53e30959b2
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-08-08 10:42:16 -0400

    Working on notifications

---
+++ BlazorStudio.ClassLib/Store/NotificationCase/NotificationStates.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Immutable;
+using Fluxor;
+
+namespace BlazorStudio.ClassLib.Store.NotificationCase;
+
+[FeatureState]
+public record NotificationStates(ImmutableList<NotificationRecord> List)
+{
+    private NotificationStates() : this(ImmutableList<NotificationRecord>.Empty)
+    {
+    }
+}
---
+++ BlazorStudio.RazorLib/Notification/NotificationDisplay.razor.cs
@@ -0,0 +1,10 @@
+﻿using BlazorStudio.ClassLib.Store.NotificationCase;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorStudio.RazorLib.Notification;
+
+public partial class NotificationDisplay : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public NotificationRecord NotificationRecord { get; set; } = null!;
+}
