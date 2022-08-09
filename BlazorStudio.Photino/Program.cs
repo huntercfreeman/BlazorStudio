32f5967b12b5a364de7970432d90d7346a10040c
Hunter Freeman <huntercfreeman@gmail.com>
2022-08-09 03:55:23 -0400

    Photino running on Linux

--- BlazorStudio.Photino/Program.cs
+++ BlazorStudio.Photino/Program.cs
@@ -1,56 +1,36 @@
-﻿using PhotinoNET;
-using System;
-using System.Drawing;
-using System.IO;
-using System.Text;
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
 
-namespace HelloPhotinoApp
+namespace BlazorStudio.Photino
 {
     class Program
     {
         [STAThread]
         static void Main(string[] args)
         {
-            // Window title declared here for visibility
-            string windowTitle = "Photino for .NET Demo App";
+            var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+            appBuilder.Services
+                .AddLogging();
 
-            // Creating a new PhotinoWindow instance with the fluent API
-            var window = new PhotinoWindow()
-                .SetTitle(windowTitle)
-                // Resize to a percentage of the main monitor work area
-                .SetUseOsDefaultSize(false)
-                .SetSize(new Size(600, 400))
-                // Center window in the middle of the screen
-                .Center()
-                .RegisterCustomSchemeHandler("app", (object sender, string scheme, string url, out string contentType) =>
-                {
-                    contentType = "text/javascript";
-                    return new MemoryStream(Encoding.UTF8.GetBytes(@"
-                        (() =>{
-                            window.setTimeout(() => {
-                                alert(`🎉 Dynamically inserted JavaScript.`);
-                            }, 1000);
-                        })();
-                    "));
-                })
-                // Most event handlers can be registered after the
-                // PhotinoWindow was instantiated by calling a registration 
-                // method like the following RegisterWebMessageReceivedHandler.
-                // This could be added in the PhotinoWindowOptions if preferred.
-                .RegisterWebMessageReceivedHandler((object sender, string message) => {
-                    var window = (PhotinoWindow)sender;
+            // register root component
+            appBuilder.RootComponents.Add<App>("app");
 
-                    // The message argument is coming in from sendMessage.
-                    // "window.external.sendMessage(message: string)"
-                    string response = $"Received message: \"{message}\"";
+            var app = appBuilder.Build();
 
-                    // Send a message back the to JavaScript event handler.
-                    // "window.external.receiveMessage(callback: Function)"
-                    window.SendWebMessage(response);
-                })
-                .Load("wwwroot/index.html"); // Can be used with relative path strings or "new URI()" instance to load a website.
+            // customize window
+            app.MainWindow
+                .SetIconFile("favicon.ico")
+                .SetTitle("Photino Hello World");
 
-            window.WaitForClose(); // Starts the application event loop
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                app.MainWindow.OpenAlertWindow("Fatal exception", error.ExceptionObject.ToString());
+            };
+
+            app.Run();
         }
+
     }
 }
