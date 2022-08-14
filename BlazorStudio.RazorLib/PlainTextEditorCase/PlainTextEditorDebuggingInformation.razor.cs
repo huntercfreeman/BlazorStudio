b19221a2db97e1b463ec1108a28eff6a319c1620
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-08-14 11:48:26 -0400

    Trying to view the syntax roots of each file

--- BlazorStudio.RazorLib/PlainTextEditorCase/PlainTextEditorDebuggingInformation.razor.cs
+++ BlazorStudio.RazorLib/PlainTextEditorCase/PlainTextEditorDebuggingInformation.razor.cs
@@ -3,6 +3,7 @@ using BlazorStudio.ClassLib.Store.SolutionCase;
 using Fluxor;
 using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.CodeAnalysis;
 using static BlazorStudio.RazorLib.PlainTextEditorCase.PlainTextEditorDisplay;
 
 namespace BlazorStudio.RazorLib.PlainTextEditorCase;
@@ -16,6 +17,35 @@ public partial class PlainTextEditorDebuggingInformation : FluxorComponent
     public IPlainTextEditor PlainTextEditor { get; set; } = null!;
     [Parameter]
     public WidthAndHeightTestResult? WidthAndHeightTestResult { get; set; }
+
+    private List<(SyntaxNode syntaxNode, string? fileName)> _documentSyntaxNodeTuples =
+        new List<(SyntaxNode syntaxNode, string? fileName)>();
+    
+    private List<(SyntaxNode syntaxNode, string? fileName)> _additionalDocumentSyntaxNodeTuples =
+        new List<(SyntaxNode syntaxNode, string? fileName)>();
+    
+    private async Task GetSyntaxRootsOnClick()
+    {
+        var currentSolutionState = SolutionStateWrap.Value;
+
+        _documentSyntaxNodeTuples.Clear();
+
+        foreach (var documentTuple in currentSolutionState.FileAbsoluteFilePathToDocumentMap)
+        {
+            var root = await documentTuple.Value.Document.GetSyntaxRootAsync();
+            
+            _documentSyntaxNodeTuples.Add((root, documentTuple.Key.AbsoluteFilePathString));
+        }
+        
+        _additionalDocumentSyntaxNodeTuples.Clear();
+
+        // foreach (var additionalDocumentTuple in currentSolutionState.FileAbsoluteFilePathToAdditionalDocumentMap)
+        // {
+        //     var root = await additionalDocumentTuple.Value.TextDocument.R.GetSyntaxRootAsync();
+        //     
+        //     _additionalDocumentSyntaxNodeTuples.Add((root, additionalDocumentTuple.Key.AbsoluteFilePathString));
+        // }
+    }
 
     private MarkupString NullSafeToMarkupString(string name, object? obj)
     {
