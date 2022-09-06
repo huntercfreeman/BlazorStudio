8c77a834be54e370ea9daa3857bcd9566a9b8709
Hunter Freeman <huntercfreeman@gmail.com>
2022-09-05 20:22:31 -0400

    Working on virtualization

--- BlazorStudio.RazorLib/VirtualizeComponentExperiments/ScrollDimensions.cs
+++
@@ -1,5 +0,0 @@
-namespace BlazorStudio.RazorLib.VirtualizeComponentExperiments;
-
-public record ScrollDimensions(
-    double ScrollLeft,
-    double ScrollTop);
--- BlazorStudio.RazorLib/VirtualizeComponentExperiments/VirtualizeItemWrapper.cs
+++
@@ -1,3 +0,0 @@
-namespace BlazorStudio.RazorLib.VirtualizeComponentExperiments;
-
-public record VirtualizeItemWrapper<TItem>(TItem Item, double TopInPixels, double WidthInPercentage);
