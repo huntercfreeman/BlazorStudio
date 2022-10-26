1f39ccdce0b81b8ce00db965218a70d51bc5026d
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-25 21:28:47 -0400

    Working on documenting nullable reference types

--- BlazorStudio.ClassLib/CustomEvents/CustomKeyDown.cs
+++ BlazorStudio.ClassLib/CustomEvents/CustomKeyDown.cs
@@ -1,9 +1,16 @@
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+//
+// Using disable UnusedAutoPropertyAccessor.Global because
+// CustomKeyDown is used by Blazor's internal event logic
+// and I don't want to touch anything regarding this.
 namespace BlazorStudio.ClassLib.CustomEvents;
 
 public class CustomKeyDown : EventArgs
 {
-    public string Key { get; set; }
-    public string Code { get; set; }
+    public string Key { get; set; } = null!;
+    public string Code { get; set; } = null!;
     public bool CtrlWasPressed { get; set; }
     public bool ShiftWasPressed { get; set; }
     public bool AltWasPressed { get; set; }
