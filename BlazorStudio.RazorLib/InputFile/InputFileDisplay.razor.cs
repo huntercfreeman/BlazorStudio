46e4b648fd5dc52b696a7c34e4876a121e9a6fed
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-27 18:41:54 -0400

    ResizableColumn InputFileDisplay.razor

--- BlazorStudio.RazorLib/InputFile/InputFileDisplay.razor.cs
+++ BlazorStudio.RazorLib/InputFile/InputFileDisplay.razor.cs
@@ -1,4 +1,5 @@
 using System.Collections.Immutable;
+using BlazorStudio.ClassLib.Dimensions;
 using BlazorStudio.ClassLib.FileSystem.Classes;
 using BlazorStudio.ClassLib.FileSystem.Interfaces;
 using BlazorStudio.ClassLib.Menu;
@@ -17,8 +18,45 @@ public partial class InputFileDisplay : ComponentBase
     private IDispatcher Dispatcher { get; set; } = null!;
     [Inject]
     private ITextEditorService TextEditorService { get; set; } = null!;
+
+    /// <summary>
+    /// Receives the <see cref="_rootTreeViewModel"/> as
+    /// as parameter to the <see cref="RenderFragment"/>
+    /// </summary>
+    [Parameter]
+    public RenderFragment<TreeViewModel<IAbsoluteFilePath>?>? HeaderRenderFragment { get; set; }
+    /// <summary>
+    /// Receives the <see cref="_rootTreeViewModel"/> as
+    /// as parameter to the <see cref="RenderFragment"/>
+    /// </summary>
+    [Parameter]
+    public RenderFragment<TreeViewModel<IAbsoluteFilePath>?>? FooterRenderFragment { get; set; }
+    /// <summary>
+    /// One would likely use <see cref="BodyClassCssString"/> in the case where
+    /// either <see cref="HeaderRenderFragment"/> or <see cref="FooterRenderFragment"/>
+    /// are being used.
+    /// <br/><br/>
+    /// This is due to one likely wanting to set a fixed height for the <see cref="HeaderRenderFragment"/>
+    /// and a fixed height for the <see cref="FooterRenderFragment"/> and lastly
+    /// the body gets a fixed height of calc(100% - HeightForHeaderRenderFragment - HeightForFooterRenderFragment); 
+    /// </summary>
+    [Parameter]
+    public string BodyClassCssString { get; set; } = null!;
+    /// <summary>
+    /// One would likely use <see cref="BodyStyleCssString"/> in the case where
+    /// either <see cref="HeaderRenderFragment"/> or <see cref="FooterRenderFragment"/>
+    /// are being used.
+    /// <br/><br/>
+    /// This is due to one likely wanting to set a fixed height for the <see cref="HeaderRenderFragment"/>
+    /// and a fixed height for the <see cref="FooterRenderFragment"/> and lastly
+    /// the body gets a fixed height of calc(100% - HeightForHeaderRenderFragment - HeightForFooterRenderFragment); 
+    /// </summary>
+    [Parameter]
+    public string BodyStyleCssString { get; set; } = null!;
     
     private TreeViewModel<IAbsoluteFilePath>? _rootTreeViewModel;
+    private ElementDimensions _navMenuElementDimensions = new();
+    private ElementDimensions _contentElementDimensions = new();
     
     protected override void OnInitialized()
     {
@@ -31,8 +69,49 @@ public partial class InputFileDisplay : ComponentBase
             LoadChildrenAsync);
 
         _rootTreeViewModel.LoadChildrenFuncAsync.Invoke(_rootTreeViewModel);
+
+        InitializeElementDimensions();
         
         base.OnInitialized();
+    }
+
+    private void InitializeElementDimensions()
+    {
+        var navMenuWidth = _navMenuElementDimensions.DimensionAttributes
+            .Single(da => da.DimensionAttributeKind == DimensionAttributeKind.Width);
+        
+        navMenuWidth.DimensionUnits.AddRange(new []
+        {
+            new DimensionUnit
+            {
+                Value = 30,
+                DimensionUnitKind = DimensionUnitKind.Percentage
+            },
+            new DimensionUnit
+            {
+                Value = 2.5,
+                DimensionUnitKind = DimensionUnitKind.Pixels,
+                DimensionOperatorKind = DimensionOperatorKind.Subtract
+            }
+        });
+
+        var contentWidth = _contentElementDimensions.DimensionAttributes
+            .Single(da => da.DimensionAttributeKind == DimensionAttributeKind.Width);
+        
+        contentWidth.DimensionUnits.AddRange(new []
+        {
+            new DimensionUnit
+            {
+                Value = 70,
+                DimensionUnitKind = DimensionUnitKind.Percentage
+            },
+            new DimensionUnit
+            {
+                Value = 2.5,
+                DimensionUnitKind = DimensionUnitKind.Pixels,
+                DimensionOperatorKind = DimensionOperatorKind.Subtract
+            }
+        });
     }
 
     private Task LoadChildrenAsync(TreeViewModel<IAbsoluteFilePath> treeViewModel)
