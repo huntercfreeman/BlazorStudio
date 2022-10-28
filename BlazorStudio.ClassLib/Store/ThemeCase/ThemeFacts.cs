56489fbc28e032bd6ef7aa1e19989eb3b3ef65c0
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-28 07:42:42 -0400

    Reflect css name changes in the ThemeFacts.cs

--- BlazorStudio.ClassLib/Store/ThemeCase/ThemeFacts.cs
+++ BlazorStudio.ClassLib/Store/ThemeCase/ThemeFacts.cs
@@ -5,12 +5,12 @@ namespace BlazorStudio.ClassLib.Store.ThemeCase;
 public static class ThemeFacts
 {
     public static readonly ThemeRecord DarkTheme = new(
-        "bte_dark-theme",
+        "bstudio_dark-theme",
         ThemeColorKind.Dark,
         ThemeContrastKind.Default);
     
     public static readonly ThemeRecord LightTheme = new(
-        "bte_light-theme",
+        "bstudio_light-theme",
         ThemeColorKind.Light,
         ThemeContrastKind.Default);
     
