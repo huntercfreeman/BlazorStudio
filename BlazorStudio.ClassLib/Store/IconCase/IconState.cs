51022232693ee7e76f9d7c36e9be6610e326e2c1
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-27 22:00:00 -0400

    Remove bte css from bstudio css file

--- BlazorStudio.ClassLib/Store/IconCase/IconState.cs
+++ BlazorStudio.ClassLib/Store/IconCase/IconState.cs
@@ -5,7 +5,7 @@ namespace BlazorStudio.ClassLib.Store.IconCase;
 [FeatureState]
 public record IconState(int IconSizeInPixels)
 {
-    public IconState() : this(20)
+    public IconState() : this(18)
     {
         
     }
