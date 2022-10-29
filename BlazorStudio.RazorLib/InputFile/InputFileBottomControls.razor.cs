d60be2e349369df2c5e9c65fcb927f1100acfd9c
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-28 20:15:26 -0400

    bottom controls

---
+++ BlazorStudio.RazorLib/InputFile/InputFileBottomControls.razor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Immutable;
+using BlazorStudio.ClassLib.Dimensions;
+using BlazorStudio.ClassLib.FileSystem.Classes;
+using BlazorStudio.ClassLib.FileSystem.Interfaces;
+using BlazorStudio.ClassLib.Menu;
+using BlazorStudio.ClassLib.Store.InputFileCase;
+using BlazorStudio.ClassLib.TreeView;
+using BlazorTextEditor.RazorLib;
+using BlazorTextEditor.RazorLib.TextEditor;
+using Fluxor;
+using Fluxor.Blazor.Web.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorStudio.RazorLib.InputFile;
+
+public partial class InputFileBottomControls : FluxorComponent
+{
+    [Inject]
+    private IState<InputFileState> InputFileStateWrap { get; set; } = null!;
+    [Inject]
+    private IDispatcher Dispatcher { get; set; } = null!;
+
+    private ElementReference? _searchElementReference;
+    private string _searchQuery = string.Empty;
+
+    private TreeViewModel<IAbsoluteFilePath> SelectionMutablyReferenced => 
+        InputFileStateWrap.Value.OpenedTreeViewModelHistory[
+            InputFileStateWrap.Value.IndexInHistory];
+    
+    private void HandleBackButtonOnClick()
+    {
+        Dispatcher.Dispatch(new InputFileState.MoveBackwardsInHistoryAction());
+    }
+    
+    private void HandleForwardButtonOnClick()
+    {
+        Dispatcher.Dispatch(new InputFileState.MoveForwardsInHistoryAction());
+    }
+
+    private void HandleUpwardButtonOnClick()
+    {
+        Dispatcher.Dispatch(new InputFileState.OpenParentDirectoryAction());
+    }
+
+    private void HandleRefreshButtonOnClick()
+    {
+        Dispatcher.Dispatch(new InputFileState.RefreshCurrentSelectionAction());
+    }
+
+    private void FocusSearchElementReferenceOnClick()
+    {
+        _searchElementReference?.FocusAsync();
+    }
+}
