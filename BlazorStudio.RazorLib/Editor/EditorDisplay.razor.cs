5b9215f3cf52b63c54b559b7933f16258e79e7da
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-29 19:35:41 -0400

    HandleAfterOnKeyDownAsync

--- BlazorStudio.RazorLib/Editor/EditorDisplay.razor.cs
+++ BlazorStudio.RazorLib/Editor/EditorDisplay.razor.cs
@@ -1,14 +1,19 @@
 using System.Collections.Immutable;
 using BlazorStudio.ClassLib.Dimensions;
+using BlazorStudio.ClassLib.Keyboard;
 using BlazorStudio.ClassLib.Store.EditorCase;
 using BlazorStudio.ClassLib.Store.FolderExplorerCase;
 using BlazorStudio.ClassLib.Store.InputFileCase;
 using BlazorStudio.ClassLib.Store.TextEditorResourceMapCase;
 using BlazorTextEditor.RazorLib;
+using BlazorTextEditor.RazorLib.Cursor;
+using BlazorTextEditor.RazorLib.HelperComponents;
 using BlazorTextEditor.RazorLib.Store.TextEditorCase;
+using BlazorTextEditor.RazorLib.TextEditor;
 using Fluxor;
 using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace BlazorStudio.RazorLib.Editor;
 
@@ -25,4 +30,41 @@ public partial class EditorDisplay : FluxorComponent
     
     [Parameter, EditorRequired]
     public ElementDimensions EditorElementDimensions { get; set; } = null!;
+    
+    private readonly SemaphoreSlim _afterOnKeyDownSyntaxHighlightingSemaphoreSlim = new(1, 1);
+    
+    private async Task HandleAfterOnKeyDownAsync(
+        TextEditorBase textEditor,
+        ImmutableArray<TextEditorCursorSnapshot> cursorSnapshots,
+        KeyboardEventArgs keyboardEventArgs,
+        Func<TextEditorMenuKind, Task> setTextEditorMenuKind)
+    {
+        var primaryCursorSnapshot = cursorSnapshots
+            .First(x =>
+                x.UserCursor.IsPrimaryCursor);
+
+        if (keyboardEventArgs.Key == ";" ||
+            KeyboardKeyFacts.IsWhitespaceCode(keyboardEventArgs.Code) ||
+            (keyboardEventArgs.CtrlKey && keyboardEventArgs.Key == "v"))
+        {
+            // Syntax Highlighting
+
+            var success = await _afterOnKeyDownSyntaxHighlightingSemaphoreSlim
+                .WaitAsync(TimeSpan.Zero);
+
+            if (!success)
+                return;
+
+            try
+            {
+                await textEditor.ApplySyntaxHighlightingAsync();
+
+                await InvokeAsync(StateHasChanged);
+            }
+            finally
+            {
+                _afterOnKeyDownSyntaxHighlightingSemaphoreSlim.Release();
+            }
+        }
+    }
 }
