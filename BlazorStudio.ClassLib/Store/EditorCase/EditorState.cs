7d5cab869e53dd25f4ff6403b1ac869aca152179
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-29 12:44:22 -0400

    EditorDisplay.razor

---
+++ BlazorStudio.ClassLib/Store/EditorCase/EditorState.cs
@@ -0,0 +1,29 @@
+﻿using BlazorTextEditor.RazorLib.TextEditor;
+using Fluxor;
+
+namespace BlazorStudio.ClassLib.Store.EditorCase;
+
+[FeatureState]
+public record EditorState(TextEditorKey ActiveTextEditorKey)
+{
+    public EditorState() : this(TextEditorKey.Empty)
+    {
+        
+    }
+
+    public record SetActiveTextEditorKeyAction(TextEditorKey TextEditorKey);
+
+    public class EditorStateReducer
+    {
+        [ReducerMethod]
+        public static EditorState ReduceSetActiveTextEditorKeyAction(
+            EditorState inEditorState,
+            SetActiveTextEditorKeyAction setActiveTextEditorKeyAction)
+        {
+            return inEditorState with
+            {
+                ActiveTextEditorKey = setActiveTextEditorKeyAction.TextEditorKey
+            };
+        }
+    }
+}
--- BlazorStudio.RazorLib/Editor/EditorDisplay.razor.cs
+++ BlazorStudio.RazorLib/Editor/EditorDisplay.razor.cs
@@ -1,15 +1,25 @@
 using BlazorStudio.ClassLib.Dimensions;
+using BlazorStudio.ClassLib.Store.EditorCase;
+using BlazorTextEditor.RazorLib;
 using BlazorTextEditor.RazorLib.Store.TextEditorCase;
 using Fluxor;
+using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazorStudio.RazorLib.Editor;
 
-public partial class EditorDisplay : ComponentBase
+public partial class EditorDisplay : FluxorComponent
 {
     [Inject]
-    private IState<TextEditorStates> TextEditorStatesWrap { get; set; } = null!;
+    private IState<EditorState> EditorStateWrap { get; set; } = null!;
+    [Inject]
+    private ITextEditorService TextEditorService { get; set; } = null!;
     
     [Parameter, EditorRequired]
     public ElementDimensions EditorElementDimensions { get; set; } = null!;
+
+    private Task OpenFileOnClick()
+    {
+        throw new NotImplementedException();
+    }
 }
