47620b3ee9a52a2e1f1611a31ecae9db667ccd61
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-31 19:47:16 -0400

    line wraping for long text in TerminalDisplay.razor

--- BlazorStudio.RazorLib/Terminal/TerminalDisplay.razor.cs
+++ BlazorStudio.RazorLib/Terminal/TerminalDisplay.razor.cs
@@ -1,4 +1,6 @@
-﻿using BlazorStudio.ClassLib.Store.TerminalCase;
+﻿using System.Text;
+using BlazorStudio.ClassLib.Html;
+using BlazorStudio.ClassLib.Store.TerminalCase;
 using Fluxor;
 using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
@@ -18,5 +20,43 @@ public partial class TerminalDisplay : FluxorComponent
             .Replace("\n", "\\n<br/>")
             .Replace("\t", "--->")
             .Replace(" ", "·");
+    }
+    
+    private static MarkupString ParseHttpLinks(string input)
+    {
+        var outputBuilder = new StringBuilder();
+
+        var indexOfHttp = input.IndexOf("http");
+
+        if (indexOfHttp > 0)
+        {
+            var firstSubstring = input.Substring(0, indexOfHttp);
+
+            var httpBuilder = new StringBuilder();
+
+            var position = indexOfHttp;
+
+            while (position < input.Length)
+            {
+                var currentCharacter = input[position++];
+
+                if (currentCharacter == ' ') break;
+
+                httpBuilder.Append(currentCharacter);
+            }
+
+            var aTag = $"<a href=\"{httpBuilder}\" target=\"_blank\">{httpBuilder}</a>";
+
+            var result = firstSubstring.EscapeHtml()
+                         + aTag;
+
+            if (position != input.Length - 1) result += input.Substring(position);
+
+            outputBuilder.Append(result + "<br />");
+        }
+        else
+            outputBuilder.Append(input.EscapeHtml() + "<br />");
+
+        return (MarkupString)(outputBuilder.ToString());
     }
 }
