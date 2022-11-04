4d6d3250e9c385c2771223a39054f2370409735c
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-11-04 14:51:55 -0400

    await MenuOptionWidgetParameters.SetShouldDisplayWidgetAsync

--- BlazorStudio.RazorLib/File/FileFormDisplay.razor.cs
+++ BlazorStudio.RazorLib/File/FileFormDisplay.razor.cs
@@ -1,11 +1,17 @@
 ﻿using BlazorStudio.ClassLib.CommonComponents;
+using BlazorStudio.ClassLib.Keyboard;
+using BlazorStudio.RazorLib.Menu;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace BlazorStudio.RazorLib.File;
 
 public partial class FileFormDisplay 
     : ComponentBase, IFileFormRendererType
 {
+    [CascadingParameter]
+    public MenuOptionWidgetParameters? MenuOptionWidgetParameters { get; set; }
+    
     [Parameter, EditorRequired]
     public string FileName { get; set; } = string.Empty;
 
@@ -37,4 +43,15 @@ public partial class FileFormDisplay
     }
 
     public string InputFileName => _fileName;
+
+    private async Task HandleOnKeyDown(KeyboardEventArgs keyboardEventArgs)
+    {
+        if (MenuOptionWidgetParameters is not null)
+        {
+            if (keyboardEventArgs.Key == KeyboardKeyFacts.MetaKeys.ESCAPE)
+            {
+                await MenuOptionWidgetParameters.SetShouldDisplayWidgetAsync.Invoke(false);
+            }
+        }
+    }
 }
