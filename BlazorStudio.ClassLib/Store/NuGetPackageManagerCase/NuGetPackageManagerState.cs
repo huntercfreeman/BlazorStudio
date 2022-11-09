ac0e0c90bcba9e9d290e9203897e1fb50a86e4f6
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-11-09 15:45:44 -0500

    Project selection

--- BlazorStudio.ClassLib/Store/NuGetPackageManagerCase/NuGetPackageManagerState.cs
+++ BlazorStudio.ClassLib/Store/NuGetPackageManagerCase/NuGetPackageManagerState.cs
@@ -1,9 +1,30 @@
 ﻿using Fluxor;
+using Microsoft.CodeAnalysis;
 
 namespace BlazorStudio.ClassLib.Store.NuGetPackageManagerCase;
 
 [FeatureState]
-public record NuGetPackageManagerState()
+public record NuGetPackageManagerState(Project? SelectedProjectToModify)
 {
+    public NuGetPackageManagerState() : this(default(Project?))
+    {
+        
+    }
+
+    public record SetSelectedProjectToModifyAction(Project? SelectedProjectToModify);
     
+    private class NuGetPackageManagerStateReducer
+    {
+        [ReducerMethod]
+        public static NuGetPackageManagerState ReduceSetSelectedProjectToModifyAction(
+            NuGetPackageManagerState inNuGetPackageManagerState,
+            SetSelectedProjectToModifyAction setSelectedProjectToModifyAction)
+        {
+            return inNuGetPackageManagerState with
+            {
+                SelectedProjectToModify = 
+                    setSelectedProjectToModifyAction.SelectedProjectToModify
+            };
+        }
+    }
 }
--- BlazorStudio.RazorLib/NuGet/NuGetPackageManager.razor.cs
+++ BlazorStudio.RazorLib/NuGet/NuGetPackageManager.razor.cs
@@ -1,8 +1,10 @@
 ﻿using BlazorStudio.ClassLib.CommonComponents;
 using BlazorStudio.ClassLib.Store.NuGetPackageManagerCase;
+using BlazorStudio.ClassLib.Store.SolutionExplorer;
 using Fluxor;
 using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.CodeAnalysis;
 
 namespace BlazorStudio.RazorLib.NuGet;
 
@@ -10,4 +12,38 @@ public partial class NuGetPackageManager : FluxorComponent, INuGetPackageManager
 {
     [Inject]
     private IState<NuGetPackageManagerState> NuGetPackageManagerStateWrap { get; set; } = null!;
+    [Inject]
+    private IState<SolutionExplorerState> SolutionExplorerStateWrap { get; set; } = null!;
+    [Inject]
+    private IDispatcher Dispatcher { get; set; } = null!;
+    
+    private void SelectedProjectToModifyChanged(
+        ChangeEventArgs changeEventArgs,
+        SolutionExplorerState solutionExplorerState)
+    {
+        if (changeEventArgs.Value is null ||
+            solutionExplorerState.Solution is null)
+        {
+            return;
+        }
+        
+        var projectGuid = Guid.Parse((string)changeEventArgs.Value);
+        
+        var projectId = ProjectId.CreateFromSerialized(projectGuid);
+
+        var selectedProject = solutionExplorerState.Solution.GetProject(projectId);
+        
+        Dispatcher.Dispatch(
+            new NuGetPackageManagerState.SetSelectedProjectToModifyAction(
+                selectedProject));       
+    }
+
+    private bool CheckIsSelected(Project project, NuGetPackageManagerState nuGetPackageManagerState)
+    {
+        if (nuGetPackageManagerState.SelectedProjectToModify is null)
+            return false;
+        
+        return nuGetPackageManagerState.SelectedProjectToModify.Id.Id.ToString() == 
+            project.Id.Id.ToString();
+    }
 }
