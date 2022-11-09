40c17ff0b61e68903816ea99d8535e69fe38ed8e
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-11-09 15:23:30 -0500

    Need to bring over old ui for NuGetPackageManager.razor

--- BlazorStudio.RazorLib/NuGet/NuGetPackageManager.razor.cs
+++ BlazorStudio.RazorLib/NuGet/NuGetPackageManager.razor.cs
@@ -1,11 +1,12 @@
 ﻿using BlazorStudio.ClassLib.CommonComponents;
 using BlazorStudio.ClassLib.Store.NuGetPackageManagerCase;
 using Fluxor;
+using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazorStudio.RazorLib.NuGet;
 
-public partial class NuGetPackageManager : ComponentBase, INuGetPackageManagerRendererType
+public partial class NuGetPackageManager : FluxorComponent, INuGetPackageManagerRendererType
 {
     [Inject]
     private IState<NuGetPackageManagerState> NuGetPackageManagerStateWrap { get; set; } = null!;
