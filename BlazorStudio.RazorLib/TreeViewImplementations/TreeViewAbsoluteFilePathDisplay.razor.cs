e5fd9881ab02af39fe824d5feedab248835441e5
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-12-26 20:48:52 -0500

    "matched"

--- BlazorStudio.RazorLib/TreeViewImplementations/TreeViewAbsoluteFilePathDisplay.razor.cs
+++ BlazorStudio.RazorLib/TreeViewImplementations/TreeViewAbsoluteFilePathDisplay.razor.cs
@@ -7,6 +7,9 @@ namespace BlazorStudio.RazorLib.TreeViewImplementations;
 public partial class TreeViewAbsoluteFilePathDisplay 
     : ComponentBase, ITreeViewAbsoluteFilePathRendererType
 {
+    [CascadingParameter(Name="SearchQuery")]
+    public string SearchQuery { get; set; } = string.Empty;
+    
     [Parameter, EditorRequired]
     public TreeViewAbsoluteFilePath TreeViewAbsoluteFilePath { get; set; } = null!;
 }
