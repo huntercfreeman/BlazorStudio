8942100d56e6c6cb5d135d014adfb65d3c40b528
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2023-01-08 11:43:42 -0500

    OnClick set active panel tab

--- BlazorStudio.RazorLib/Panel/PanelTabDisplay.razor.cs
+++ BlazorStudio.RazorLib/Panel/PanelTabDisplay.razor.cs
@@ -1,10 +1,28 @@
 ﻿using BlazorStudio.ClassLib.Panel;
+using BlazorStudio.ClassLib.Store.PanelCase;
+using Fluxor;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazorStudio.RazorLib.Panel;
 
 public partial class PanelTabDisplay : ComponentBase
 {
+    [Inject]
+    private IDispatcher Dispatcher { get; set; } = null!;
+    
     [Parameter, EditorRequired]
     public PanelTab PanelTab { get; set; } = null!;
+    [Parameter, EditorRequired]
+    public PanelRecord PanelRecord { get; set; } = null!;
+
+    private string IsActiveCssClassString => PanelRecord.ActivePanelTabKey == PanelTab.PanelTabKey
+        ? "balc_active"
+        : string.Empty;
+
+    private void DispatchSetActivePanelTabActionOnClick()
+    {
+        Dispatcher.Dispatch(new PanelsCollection.SetActivePanelTabAction(
+            PanelRecord.PanelRecordKey,
+            PanelTab.PanelTabKey));
+    }
 }
