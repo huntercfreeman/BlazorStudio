3e863bb267a1891f271b5f1d09f9396b167edf15
Hunter Freeman <huntercfreeman@gmail.com>
2023-03-23 08:54:46 -0400

    Runs

--- BlazorStudio.ClassLib/CommonComponents/CommonComponentRenderers.cs
+++ BlazorStudio.ClassLib/CommonComponents/CommonComponentRenderers.cs
@@ -18,98 +18,20 @@ public class CommonComponentRenderers : ICommonComponentRenderers
         Type? removeCSharpProjectFromSolutionRendererType,
         Type? booleanPromptOrCancelRendererType)
     {
-        // Validate
-        {
-            ValidateRenderer(
-                inputFileRenderer, 
-                typeof(IInputFileRendererType),
-                t => InputFileRendererType = t);
-                
-            ValidateRenderer(
-                informativeNotificationRenderer, 
-                typeof(IInformativeNotificationRendererType),
-                t => InformativeNotificationRendererType = t);
-                
-            ValidateRenderer(
-                errorNotificationRenderer, 
-                typeof(IErrorNotificationRendererType),
-                t => ErrorNotificationRendererType = t);
-                
-            ValidateRenderer(
-                fileFormRendererType, 
-                typeof(IFileFormRendererType),
-                t => FileFormRendererType = t);
-            
-            ValidateRenderer(
-                deleteFileFormRendererType, 
-                typeof(IDeleteFileFormRendererType),
-                t => DeleteFileFormRendererType = t);
-            
-            ValidateRenderer(
-                treeViewMissingRendererFallbackType, 
-                typeof(ITreeViewMissingRendererFallbackType),
-                t => TreeViewMissingRendererFallbackType = t);
-            
-            ValidateRenderer(
-                treeViewNamespacePathRendererType, 
-                typeof(ITreeViewNamespacePathRendererType),
-                t => TreeViewNamespacePathRendererType = t);
-            
-            ValidateRenderer(
-                treeViewExceptionRendererType, 
-                typeof(ITreeViewExceptionRendererType),
-                t => TreeViewExceptionRendererType = t);
-            
-            ValidateRenderer(
-                treeViewAbsoluteFilePathRendererType, 
-                typeof(ITreeViewAbsoluteFilePathRendererType),
-                t => TreeViewAbsoluteFilePathRendererType = t);
-            
-            ValidateRenderer(
-                treeViewGitFileRendererType, 
-                typeof(ITreeViewGitFileRendererType),
-                t => TreeViewGitFileRendererType = t);
-            
-            ValidateRenderer(
-                nuGetPackageManagerRendererType, 
-                typeof(INuGetPackageManagerRendererType),
-                t => NuGetPackageManagerRendererType = t);
-            
-            ValidateRenderer(
-                gitDisplayRendererType, 
-                typeof(IGitDisplayRendererType),
-                t => GitDisplayRendererType = t);
-            
-            ValidateRenderer(
-                removeCSharpProjectFromSolutionRendererType, 
-                typeof(IRemoveCSharpProjectFromSolutionRendererType),
-                t => RemoveCSharpProjectFromSolutionRendererType = t);
-            
-            ValidateRenderer(
-                booleanPromptOrCancelRendererType, 
-                typeof(IBooleanPromptOrCancelRendererType),
-                t => BooleanPromptOrCancelRendererType = t);
-        }
-        
-        void ValidateRenderer(
-            Type? implementationType,
-            Type interfaceType,
-            Action<Type?> setPropertyFunc)
-        {
-            // If "implementationType is null" then the UI Element is not in use and can be skipped.
-            if (implementationType is null)
-                return;
-            
-            if (implementationType.GetInterface(interfaceType.Name) is null)
-            {
-                throw new ApplicationException(
-                    $"The {nameof(implementationType)} provided: {implementationType.Name}''" +
-                    $" does not implement the required interface:" +
-                    $" {nameof(interfaceType.Name)}");
-            }
-
-            setPropertyFunc.Invoke(implementationType);
-        }
+        InputFileRendererType = inputFileRenderer;
+        InformativeNotificationRendererType = informativeNotificationRenderer;
+        ErrorNotificationRendererType = errorNotificationRenderer;
+        FileFormRendererType = fileFormRendererType;
+        DeleteFileFormRendererType = deleteFileFormRendererType;
+        TreeViewMissingRendererFallbackType = treeViewMissingRendererFallbackType;
+        TreeViewNamespacePathRendererType = treeViewNamespacePathRendererType;
+        TreeViewExceptionRendererType = treeViewExceptionRendererType;
+        TreeViewAbsoluteFilePathRendererType = treeViewAbsoluteFilePathRendererType;
+        TreeViewGitFileRendererType = treeViewGitFileRendererType;
+        NuGetPackageManagerRendererType = nuGetPackageManagerRendererType;
+        GitDisplayRendererType = gitDisplayRendererType;
+        RemoveCSharpProjectFromSolutionRendererType = removeCSharpProjectFromSolutionRendererType;
+        BooleanPromptOrCancelRendererType = booleanPromptOrCancelRendererType;
     }
 
     public Type? InputFileRendererType { get; private set; }
