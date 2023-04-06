9a20ec802e3bbea51b4c4e7f63327c18e465298c
Hunter Freeman <huntercfreeman@gmail.com>
2023-04-05 23:55:37 -0400

    XmlParser - attribute name and value were returned

--- BlazorStudio.ClassLib/Xml/XmlParser.cs
+++ BlazorStudio.ClassLib/Xml/XmlParser.cs
@@ -71,22 +71,43 @@ public class XmlParser
         StringWalker stringWalker,
         XmlDiagnosticBag xmlDiagnosticBag)
     {
-        var tagNameBuilder = new StringBuilder();
+        /*
+         * <div class="mns_blue-background">
+         *     Text Node
+         *     <span>Text Node</span>
+         * </div>
+         */
         
-        while (!stringWalker.IsEof)
-        {
-            
-            
-            _ = stringWalker.ReadCharacter();
-        }
+        var openTagSyntax = ParseOpenTagSyntax(
+            stringWalker,
+            xmlDiagnosticBag);
+        
+        var attributeSyntaxes = ParseAttributeSyntaxesHelper(
+            stringWalker,
+            xmlDiagnosticBag);
 
-        var openTagSyntax = ParseOpenTagSyntax();
+        var contentLength = stringWalker.Content.Length;
+
+        var debuggingAttributes = attributeSyntaxes
+            .Select(x =>
+            (
+                stringWalker.Content.Substring(
+                    x.AttributeNameSyntax.TextEditorTextSpan.StartingIndexInclusive,
+                    x.AttributeNameSyntax.TextEditorTextSpan.EndingIndexExclusive -
+                    x.AttributeNameSyntax.TextEditorTextSpan.StartingIndexInclusive),
+                stringWalker.Content.Substring(
+                    x.AttributeValueSyntax.TextEditorTextSpan.StartingIndexInclusive,
+                    x.AttributeValueSyntax.TextEditorTextSpan.EndingIndexExclusive -
+                    x.AttributeValueSyntax.TextEditorTextSpan.StartingIndexInclusive)))
+            .ToArray();
         
-        var attributeSyntaxes = ParseAttributeSyntaxes();
+        var childXmlSyntaxes = ParseChildXmlSyntaxes(
+            stringWalker,
+            xmlDiagnosticBag);
         
-        var childXmlSyntaxes = ParseChildXmlSyntaxes();
-        
-        var closeTagSyntax = ParseCloseTagSyntax();
+        var closeTagSyntax = ParseCloseTagSyntax(
+            stringWalker,
+            xmlDiagnosticBag);
 
         return new TagSyntax(
             openTagSyntax,
@@ -96,22 +117,139 @@ public class XmlParser
             TagKind.Opening);
     }
 
-    private static TagNameSyntax ParseOpenTagSyntax()
+    private static TagNameSyntax ParseOpenTagSyntax(
+        StringWalker stringWalker,
+        XmlDiagnosticBag xmlDiagnosticBag)
     {
+        var startingPositionIndex = stringWalker.PositionIndex;
+        
+        var tagNameBuilder = new StringBuilder();
+        
+        while (!stringWalker.IsEof)
+        {
+            if (WhitespaceFacts.ALL.Contains(stringWalker.CurrentCharacter) ||
+                stringWalker.CheckForSubstring(XmlFacts.OPEN_TAG_WITH_CHILD_CONTENT_ENDING) ||
+                stringWalker.CheckForSubstring(XmlFacts.OPEN_TAG_SELF_CLOSING_ENDING))
+            {
+                break;
+            }
+
+            tagNameBuilder.Append(stringWalker.CurrentCharacter);
+            
+            _ = stringWalker.ReadCharacter();
+        }
+
+        var textEditorTextSpan = new TextEditorTextSpan(
+            startingPositionIndex,
+            stringWalker.PositionIndex,
+            0);
+        
+        return new TagNameSyntax(
+            tagNameBuilder.ToString(),
+            textEditorTextSpan);
+    }
+    
+    private static ImmutableArray<AttributeSyntax> ParseAttributeSyntaxesHelper(
+        StringWalker stringWalker,
+        XmlDiagnosticBag xmlDiagnosticBag)
+    {
+        var startingPositionIndex = stringWalker.PositionIndex;
+        var attributeSyntaxes = new List<AttributeSyntax>();
+        
+        while (!stringWalker.IsEof)
+        {
+            if (WhitespaceFacts.ALL.Contains(stringWalker.CurrentCharacter))
+            {
+                _ = stringWalker.ReadCharacter();
+                continue;
+            }
+            
+            if (stringWalker.CheckForSubstring(XmlFacts.OPEN_TAG_WITH_CHILD_CONTENT_ENDING) ||
+                     stringWalker.CheckForSubstring(XmlFacts.OPEN_TAG_SELF_CLOSING_ENDING))
+            {
+                break;
+            }
+
+            var attributeSyntax = ParseAttributeSyntax(
+                stringWalker,
+                xmlDiagnosticBag);
+
+            attributeSyntaxes.Add(attributeSyntax);
+        }
+
+        return attributeSyntaxes.ToImmutableArray();
+    }
+
+    private static AttributeSyntax ParseAttributeSyntax(
+        StringWalker stringWalker,
+        XmlDiagnosticBag xmlDiagnosticBag)
+    {
+        var startingPositionIndex = stringWalker.PositionIndex;
+        
+        int? attributeNameStartingPositionIndexInclusive = null;
+        int? attributeNameEndingPositionIndexExclusive = null;
+        
+        int? attributeValueStartingPositionIndexInclusive = null;
+        int? attributeValueEndingPositionIndexExclusive = null;
+        
+        while (!stringWalker.IsEof)
+        {
+            if (WhitespaceFacts.ALL.Contains(stringWalker.CurrentCharacter) ||
+                stringWalker.CheckForSubstring(XmlFacts.OPEN_TAG_WITH_CHILD_CONTENT_ENDING) ||
+                stringWalker.CheckForSubstring(XmlFacts.OPEN_TAG_SELF_CLOSING_ENDING))
+            {
+                if (attributeNameEndingPositionIndexExclusive is null)
+                    attributeNameEndingPositionIndexExclusive = stringWalker.PositionIndex;
+                else
+                {
+                    attributeValueEndingPositionIndexExclusive = stringWalker.PositionIndex;
+                }
+            }
+
+            if (attributeNameStartingPositionIndexInclusive is null)
+                attributeNameStartingPositionIndexInclusive = stringWalker.PositionIndex;
+            else
+                attributeValueStartingPositionIndexInclusive = stringWalker.PositionIndex;
+            
+            _ = stringWalker.ReadCharacter();
+        }
+
+        var attributeNameSyntax = new AttributeNameSyntax(
+            new TextEditorTextSpan(
+                attributeNameStartingPositionIndexInclusive.Value,
+                attributeNameEndingPositionIndexExclusive.Value,
+                0));
+        
+        var attributeValueSyntax = new AttributeValueSyntax(
+            new TextEditorTextSpan(
+                attributeValueStartingPositionIndexInclusive.Value,
+                attributeValueEndingPositionIndexExclusive.Value,
+                0));
+
+        var attributeSyntax = new AttributeSyntax(
+            attributeNameSyntax,
+            attributeValueSyntax);
+        
+        return attributeSyntax;
+    }
+
+    private static ImmutableArray<IXmlSyntax> ParseChildXmlSyntaxes(
+        StringWalker stringWalker,
+        XmlDiagnosticBag xmlDiagnosticBag)
+    {
+        /*
+         * <div class="mns_blue-background">
+         *     Text Node
+         *     <span>Text Node</span>
+         * </div>
+         */
+
         throw new NotImplementedException();
     }
     
-    private static ImmutableArray<AttributeSyntax> ParseAttributeSyntaxes()
-    {
-        throw new NotImplementedException();
-    }
-    
-    private static ImmutableArray<IXmlSyntax> ParseChildXmlSyntaxes()
-    {
-        throw new NotImplementedException();
-    }
-    
-    private static TagNameSyntax ParseCloseTagSyntax()
+    private static TagNameSyntax ParseCloseTagSyntax(
+        StringWalker stringWalker,
+        XmlDiagnosticBag xmlDiagnosticBag)
     {
         throw new NotImplementedException();
     }
