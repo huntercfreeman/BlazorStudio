c6f256689db21079701be1196fd7f5d19e6b2e4f
Hunter Freeman <huntercfreeman@gmail.com>
2023-04-09 21:15:45 -0400

    Bring back NuGet Package Manager from Git History

---
+++ BlazorStudio.ClassLib/Store/NugetPackageManagerCase/NuGetPackageManagerState.Actions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Immutable;
+using BlazorStudio.ClassLib.DotNet;
+using BlazorStudio.ClassLib.Nuget;
+using Fluxor;
+using Microsoft.CodeAnalysis;
+
+namespace BlazorStudio.ClassLib.Store.NuGetPackageManagerCase;
+
+public partial record NuGetPackageManagerState
+{    
+    public record SetSelectedProjectToModifyAction(IDotNetProject? SelectedProjectToModify);
+    public record SetNugetQueryAction(string NugetQuery);
+    public record SetIncludePrereleaseAction(bool IncludePrerelease);
+    public record SetMostRecentQueryResultAction(ImmutableArray<NugetPackageRecord> QueryResult);
+}
---
+++ BlazorStudio.ClassLib/Store/NugetPackageManagerCase/NuGetPackageManagerState.Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using BlazorStudio.ClassLib.DotNet;
+using BlazorStudio.ClassLib.Nuget;
+using Fluxor;
+using Microsoft.CodeAnalysis;
+
+namespace BlazorStudio.ClassLib.Store.NuGetPackageManagerCase;
+
+[FeatureState]
+public partial record NuGetPackageManagerState(
+    IDotNetProject? SelectedProjectToModify, 
+    string NugetQuery,
+    bool IncludePrerelease,
+    ImmutableArray<NugetPackageRecord> MostRecentQueryResult)
+{
+    public NuGetPackageManagerState() 
+        : this(
+            default(IDotNetProject?), 
+            string.Empty, 
+            false, 
+            ImmutableArray<NugetPackageRecord>.Empty)
+    {
+        
+    }
+}
